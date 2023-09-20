e95d4a36fe1ac46c7b37201f7e7722e76eb31ae9
Philip Esiape <Esiape91@gmail.com>
2023-09-19 22:41:50 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,33 @@
 import './App.css';
-import { useState } from 'react';
-import Task from './Task';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
 function App(){
-    const [todoList, setTodolist] =useState([]);
-    const [newTask, setNewTask] = useState("")
+const [universities,setUniversities] = useState([])
 
-  const handleChange = (event) =>{
-      setNewTask(event.target.value)
-    }
+useEffect(()=>{
+  axios
+    .get('http://universities.hipolabs.com/search?country=United+States')
+  .then((response)=>{
+    setUniversities(response.data)
+   })
+   .catch((error)=>{
+    console.log(error)
 
-  const addTask =()=>{
+   })
+}, [] )
 
-    const task ={
-      id: todoList.length ===0? 1: todoList[todoList.length-1].id +1,
-      taskName: newTask,
-      completed: false,
-    }
-    setTodolist([...todoList, newTask])
-  }
-  
-  const deleteTask =(id)=>{
-    setTodolist(todoList.filter((task)=>task.id !==id));
-  }
+return(
+   <div>
+      <h1>Fetching of data</h1>
+      <h2>universities in the United States</h2>
+      <ul>
+        {universities.map((university)=>(
+          <li key={university.name}>{university.name}</li>
+        ))}
+      </ul>
 
-
-    const completeTask=(id)=>{
-      setTodolist(
-        todoList.map((task)=>{
-          if(task.id === id){
-            return{...task, completed: true}
-          } else{
-            return task;
-          }
-
-        })
-      
-      )
-  
-  }
-  return(
-           <div className='App'>
-               <div className='addTask'>
-                <p>TODO LIST</p>
-                <input placeholder=' Write your to do list...'onChange={handleChange} />
-                <button onClick={addTask}>Add Task</button>
-                
-                <p>LIST OF WORKS</p>
-               <div className='list'>
-                  {todoList.map((task)=>{
-                      return(
-                        <Task taskName = {task.taskName} 
-                        id={task.id}
-                        completed= {task.completed} 
-                        deleteTask = {deleteTask}
-                        completeTask = {completeTask}/>
-                    
-                      )
-
-                  })}
-               </div>
-           </div>
-
-    </div>
-  )
+   </div>
+ )
 }
 export default App;
